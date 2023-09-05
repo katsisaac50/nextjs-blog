e37df548883b8a23f4a999c039ad052a1402b0c3
katongole <katsisaac50@gmail.com>
2023-09-05 08:45:25 +0200

    update post component to render contentHtml using dangerouslySetInnerHTML

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,8 +9,10 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
-  )
+  );
 }
 
 export async function getStaticPaths() {
